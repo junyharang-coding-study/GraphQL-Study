41aff238cd44809e6db6d14bea05f017de93ec55
JunyHarang <junyharang8592@gmail.com>
2023-11-28 15:30:30 +0900

    feat: Nest.js GraphQL 실습 초기 환경 구성 완료

---
+++ type-script-nest-graph-ql-실습/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: ":memory",
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+    }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
